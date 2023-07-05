f50980b1bc47e58c0da3f825bf6efc066afcbe61
Sergey Indenbom <dzorogh@gmail.com>
2023-07-05 10:34:25 +0300

    feat: copy packages to granular control of script at beta version

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,5 @@
-import { stripUtmFromRequest } from "cf-strip-utm"
-import { addParamFromCookie } from "add-param-from-cookie-cf"
+import {addParamFromCookie} from "add-param-from-cookie-cf"
+import {parse} from "cookie"
 
 const bitrixCityCookieName = "BITRIX_SM_ARG_CITY";
 const bitrixCityDefault = "1";
@@ -24,4 +24,51 @@ async function handleRequest(request: Request) {
 	);
 
 	return modifiedResponse;
+}
+
+async function addParamToPreventCityDetection(request: Request) {
+	let url = new URL(request.url)
+
+	url = setSearchParam(url, 'PREVENT_CITY', '1')
+
+	return new Request(url, request)
+}
+
+function setSearchParam(url: URL, name: string, value: string,) {
+	url.searchParams.set(name, value);
+
+	return url
+}
+
+function getCookie(request: Request, cookieName: string): string | null {
+	const cookie = parse(request.headers.get("Cookie") || "");
+	if (cookie[cookieName] != null) {
+		return String(cookie[cookieName])
+	} else {
+		return null
+	}
+}
+
+const urlRegex = new RegExp('(yclid|gclid|_openstat|utm_(source|campaign|medium|term|content))');
+
+async function stripUtmFromRequest(request: Request) {
+	let url = new URL(request.url)
+
+	url = await normalizeUrl(url)
+
+	return new Request(url, request)
+}
+
+function normalizeUrl(url: URL) {
+	let deleteKeys = []
+
+	for (const key of url.searchParams.keys()) {
+		if (key.match(urlRegex)) {
+			deleteKeys.push(key)
+		}
+	}
+
+	deleteKeys.forEach(k => url.searchParams.delete(k))
+
+	return url
 }
